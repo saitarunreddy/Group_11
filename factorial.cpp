c1508a32f170deb11f8d4ea940fcc5fd6085aed6
Sai Tarun Reddy <tarun@Sais-MacBook-Pro.local>
2017-08-22 16:59:01 +0530

    negative number check

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,16 @@
 #include "functions.h"
 
-int factorial(int n){
-    if(n!=1){
-	return(n * factorial(n-1));
+int factorial(int n)
+{
+	if(n<0)
+	{
+		printf("Enter a positive number\n");
+    	return 0;
+    }
+    
+    if(n!=1)
+    {
+		return(n * factorial(n-1));
     }
     else return 1;
 }
